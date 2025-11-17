be0d9ba023b451606fac25427090068dbe5e3b2b
mrezahh <m.rezahh@gmail.com>
2025-11-17 22:04:06 +0100

    Login Featuers is added.

--- core/config.py
+++ core/config.py
@@ -1,8 +1,8 @@
 
-# Database URL
+# Database URL Constant
 DATABASE_URL = "postgresql://postgres:0000@localhost:5432/resume_ai"
 
-# JWT settings
+# JWT settings Constants
 SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60*24  # 1 day
